d70eeed9e6f102a55c3695f8f4261aa1927a1700
Raju Ranjan Kushwaha <ranjanrajukumar4@gmail.com>
2022-12-12 22:42:04 +0530

    Crud Operation asp.net mvc

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,129 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //var objCategoryList=_db.Categories.ToList();
+            //return View();
+
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+
+
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+           return View();
+
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "error");
+            }
+
+            if (ModelState.IsValid)
+                {
+                    _db.Categories.Add(obj);
+                    _db.SaveChanges(); //go to database
+                     TempData["success"] = "Category Create successfully.";
+                    return RedirectToAction("Index", "Category");
+                }
+            
+            return View(obj);   
+        }
+
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(Id);
+            //var categoryFromDb=_db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDbFirst=_db.Categories.SingleOrDefault(c => c.Id == id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "error");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges(); //go to database
+                TempData["success"] = "Category Update successfully.";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(Id);
+            //var categoryFromDb=_db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDbFirst=_db.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+
+        }
+
+        //Post
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? Id)
+        {
+            var obj = _db.Categories.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _db.Categories.Remove(obj);
+                _db.SaveChanges(); //go to database
+            TempData["success"] = "Category Delete successfully.";
+            return RedirectToAction("Index");
+            
+
+        }
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Please enter between 1 t0 100number")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }= DateTime.Now;
+    }
+}
